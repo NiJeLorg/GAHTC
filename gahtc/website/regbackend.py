d95f9497cad756ffabd754da662a01641725c10f
JD Godchaux <jd@nijel.org>
2016-01-06 13:28:23 -0500

    error fix for registration with new forms

--- gahtc/website/regbackend.py
+++ gahtc/website/regbackend.py
@@ -8,12 +8,11 @@ class MyRegistrationView(RegistrationView):
 
     def register(self, request, form_class):
         new_user = super(MyRegistrationView, self).register(request, form_class)
-        user_profile = profile()
-        user_profile.user = new_user
+        user_profile = profile.objects.get(user=new_user)
         user_profile.name = form_class.cleaned_data['name']
         user_profile.institution = form_class.cleaned_data['institution']
         user_profile.teaching = form_class.cleaned_data['teaching']
         user_profile.introduction = form_class.cleaned_data['introduction']
-        user_profile.pic = form_class.cleaned_data['pic']
+        user_profile.avatar = form_class.cleaned_data['avatar']
         user_profile.save()
         return user_profile
