014e3a04ae36dcde85dd2826e804b5aa11ac03ce
JD Godchaux <jd@nijel.org>
2016-11-07 18:28:40 -0500

    Update controllers.js

--- gahtc/website/static/website/js/controllers.js
+++ gahtc/website/static/website/js/controllers.js
@@ -285,7 +285,7 @@ $( document ).ready(function() {
 		// check which image we're on an go to the next one
 		if ($('#searchFor').hasClass('oldDispensary')) {
 			// set new image background
-			$("#background li").css({ "background-image": "url('images/Palace_of_Sargon_II.JPG')"});
+			$("#background li").css({ "background": "url('/static/website/css/images/Palace_of_Sargon_II.JPG')"});
 			// set class and text for search for button
 			$('#searchFor').removeClass('oldDispensary');
 			$('#searchFor').addClass('sargon');
