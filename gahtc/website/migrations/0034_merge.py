7db22b3cfd3d4b1a55519bb2206fc76be06bf294
JD Godchaux <jd@nijel.org>
2016-11-12 13:17:14 -0500

    update migrations

---
+++ gahtc/website/migrations/0034_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('website', '0032_auto_20161108_0210'),
+        ('website', '0033_profile_verified'),
+    ]
+
+    operations = [
+    ]
