cea535bb754b6b0daf81d9ce901f6549d74e460f
JD Godchaux <jd@nijel.org>
2017-06-13 17:14:05 -0400

    editing files and filenames

--- gahtc/website/static/website/js/inline-editing.js
+++ gahtc/website/static/website/js/inline-editing.js
@@ -7,40 +7,73 @@ $( document ).ready(function() {
 
 	// on click of lecture, run query to pull lecture
 	$( ".editable-title" ).click(function(e) {
+		var sel = this;
+		var field = 'title';
+		inlineEdit(sel, field);
+	});
+
+	// on click of lecture, run query to pull lecture
+	$( ".editable-doc" ).click(function(e) {
+		var sel = this;
+		var field = 'doc';
+		inlineEdit(sel, field);
+	});
+
+	function inlineEdit(sel, field) {
 		// selectors
-		var statictext = $(this).children('.static');
-		var edittext = $(this).children('.edit');
+		var statictext = $(sel).children('.static');
+		var edittext = $(sel).children('.edit');
 
 		// allow editing
 		statictext.addClass('hidden');
 		edittext.removeClass('hidden');
 
 		// what type of title is this?
-		if ($(this).data( "moduledocid" )) {
+		if ($(sel).data( "moduledocid" )) {
 			var type = 'moduledoc';
-			var itemid = $(this).data( "moduledocid" );
-		} else if ($(this).data( "lectureid" )) {
+			var itemid = $(sel).data( "moduledocid" );
+		} else if ($(sel).data( "lectureid" )) {
 			var type = 'lecture';
-			var itemid = $(this).data( "lectureid" );
-		} else if ($(this).data( "lecturesegmentid" )) {
+			var itemid = $(sel).data( "lectureid" );
+		} else if ($(sel).data( "lecturesegmentid" )) {
 			var type = 'lecturesegment';
-			var itemid = $(this).data( "lecturesegmentid" );
-		} else if ($(this).data( "lecturedocumentid" )) {
+			var itemid = $(sel).data( "lecturesegmentid" );
+		} else if ($(sel).data( "lecturedocumentid" )) {
 			var type = 'lecturedocument';
-			var itemid = $(this).data( "lecturedocumentid" );
+			var itemid = $(sel).data( "lecturedocumentid" );
 		} 
 
 		// listen for keypresses and save when enter is pressed.
 		var edittextinput = edittext.children('input');
-		edittextinput.keypress(function(e) {
+		edittextinput.unbind().bind("keypress", function(e) {
 			if (e.which == 13) {
 				//get contents of input
 				var new_text = edittextinput.val();
 
+				// for document file names, do a bit of validation
+				var valid = true;
+				if (type == 'moduledoc' || type == 'lecturedocument') {
+					valid = validate(new_text, 'doc');
+				} else {
+					valid = validate(new_text, 'pptx');
+				}
+
+				// stop executing the script if filename isn't valid
+				if (!valid) {
+					return;
+				}
+				
 				// save contents to database
+				var url;
+				if (field == 'title') {
+					url = "/inline_edit_update_title/?new_text=" + new_text + "&type=" + type + "&itemid=" + itemid
+				} else {
+					url = "/inline_edit_update_doc/?new_text=" + new_text + "&type=" + type + "&itemid=" + itemid
+				}
+
 				$.ajax({
 					type: "GET",
-					url: "/inline_edit_update_title/?new_text=" + new_text + "&type=" + type + "&itemid=" + itemid,
+					url: url,
 					success: function(data){
 						console.log(data);
 			        }
@@ -54,12 +87,34 @@ $( document ).ready(function() {
 				statictext.removeClass('hidden');
 
 			}
-		});
+		});		
+	}
 
-	});
+	function validate(new_text, test) {
+		var validFilename = /^[a-z0-9_.@()-]+\.[^.]+$/i.test(new_text);
+		if (!validFilename) {
+			alert('The file name you entered has either spaces or an odd character. Please check the file name and try again.');
+			return false;
+		}
 
-	function updateTitle(new_text, type, itemid) {
+		if (test == 'doc') {
+			var isDocxOrPdf = /\.(docx|pdf)$/i.test(new_text);
+			if (!isDocxOrPdf) {
+				alert('The file name doesn not end in ".docx" or ".pdf". Please check the file name and try again.');
+				return false;
+			}
+		}
+		
+		if (test == 'pptx') {
+			var isPptx = /\.pptx$/i.test(new_text);
+			if (!isPptx) {
+				alert('The file name doesn not end in ".pptx". Please check the file name and try again.');
+				return false;
+			}
+		}
 
+		return true;
+	
 	}
 
 
