1312207a4c0963cc22f0e6ff1600d5e9efba019f
JD Godchaux <jd@nijel.org>
2017-07-05 19:55:50 -0400

    creating a filter widget for authors

--- gahtc/website/management/commands/assign_authorship.py
+++ gahtc/website/management/commands/assign_authorship.py
@@ -40,11 +40,31 @@ class Command(BaseCommand):
                 except Exception as e:
                     print e
 
+    def assign_cs_modules(self):
+        modules_objects = comingSoonModules.objects.all()
+        
+        for m in modules_objects:
+            # try to look up author last name in profiles, and if it exists, assign it to the module
+            author_names_string = string.replace(m.authors, ',', '')
+            author_names = author_names_string.strip().split()
+            for name in author_names:          
+                try:
+                    profile_object = profile.objects.get(last_name=name)
+                    if profile_object:
+                        m.authors_m2m.add(profile_object)
+                        #set as contributing member
+                        profile_object.contributing = True
+                        profile_object.save()
+                except Exception as e:
+                    print e
+
     def handle(self, *args, **options):
         print "Assign Authorship to Modules...."
         self.assign_modules()
         print "Assign Authorship to Lectures...."
         self.assign_lectures()
+        print "Assign Authorship to Coming Soon Modules...."
+        self.assign_cs_modules()
         print "Done."
 
 
