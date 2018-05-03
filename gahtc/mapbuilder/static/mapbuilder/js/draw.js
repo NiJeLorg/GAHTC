decfead96011d6cda6af64ad7641f2704e1c3b10
judithachieng <judith.achieng@andela.com>
2018-05-03 03:23:16 +0300

    feat(draw-toolbar): fix freedraw isdraw mode

--- gahtc/mapbuilder/static/mapbuilder/js/draw.js
+++ gahtc/mapbuilder/static/mapbuilder/js/draw.js
@@ -14,9 +14,16 @@ $('#rect').click(function(){
     });
 canvasF.add(rect).setActiveObject(rect);
 })
+
+
+
+
 $('#freedraw').click(function(){
     canvasF.isDrawingMode = true;
-    canvasF.freeDrawingBrush.width = 10;
+    canvasF.freeDrawingBrush.width = 6;
+    canvasF.on('mouse:up', function () {
+        canvasF.isDrawingMode = false;
+    });
 })
 $('#line').click(function(){
     var line = new fabric.Line([50, 100, 200, 200], {
