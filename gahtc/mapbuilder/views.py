abc30371e48b2c6166f218bafa5487423e13b533
judithachieng <judith.achieng@andela.com>
2018-05-10 23:32:26 +0300

    feat(mapbuilder): add data model for mapbuilder

--- gahtc/mapbuilder/views.py
+++ gahtc/mapbuilder/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import json
 from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Map
 
 from django.http import HttpResponse
 
@@ -14,11 +15,9 @@ from django.http import HttpResponse
 @login_required
 def index(request):
 
-	"""
-	  Index page
-	"""
+	maps = Map.objects.filter(public=True).order_by('created_date')
 
-	return render(request, 'mapbuilder/index.html')
+	return render(request, 'mapbuilder/index.html',{'maps':maps})
 
 @login_required
 def startmap(request):
@@ -52,9 +51,17 @@ def mapextent(request):
 @login_required
 def mapexport(request):
 
-
-	"""
-	  map page
-	"""
-
-	return render(request, 'mapbuilder/map-export.html')
+		if request.method == 'POST' and request.is_ajax():
+				map_id = request.POST.get('map_id')
+				if map_id == '':
+					map_id = None
+				else:
+					map_id =int(map_id)
+				map_data = request.POST.get('map_data')
+				map_name = request.POST.get('map_name')
+				map, created = Map.objects.get_or_create(id=map_id, user=request.user)
+				map.data = map_data
+				map.name = map_name
+				map.save()
+				return HttpResponse(json.dumps({'map_id': map.id}), content_type="application/json")
+		return render(request, 'mapbuilder/map-export.html')
